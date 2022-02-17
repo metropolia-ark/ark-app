3f3f48ef3d2a99202c30c803e5604476bf283b04
András Ádám <adm.andras@gmail.com>
2022-02-17 11:58:50 +0200

    Create form input and button components

---
+++ src/components/FormButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, ButtonProps, Spinner } from '@ui-kitten/components';
+import { useFormikContext } from 'formik';
+
+type FormValues = Record<string, never>;
+
+interface FormButtonProps extends ButtonProps {
+  children: string;
+}
+
+// Button component that hooks into Formik's context to automatically submit
+// the form and display the loading status. Note: the component requires the
+// Formik context to be in scope, so it only works within a Formik component.
+const FormButton = ({ children, ...props }: FormButtonProps) => {
+  const formik = useFormikContext<FormValues>();
+  return (
+    <Button
+      {...props}
+      disabled={formik.isSubmitting || props.disabled}
+      onPress={formik.submitForm || props.onPress}
+      accessoryLeft={formik.isSubmitting ? () => <Spinner size="tiny" /> : props.accessoryLeft}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default FormButton;
---
+++ src/components/FormInput.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Input, InputProps, Text } from '@ui-kitten/components';
+import { useFormikContext } from 'formik';
+
+type FormValues = Record<string, never>;
+
+interface FormInputProps extends InputProps {
+  name: string;
+  label: string;
+}
+
+// Input component that hooks into Formik's context to automatically update
+// the form data and display error messages. Note: the component requires the
+// Formik context to be available, so it only works within a Formik component.
+const FormInput = ({ name, ...props }: FormInputProps) => {
+  const formik = useFormikContext<FormValues>();
+  const showError = formik.touched[name] && formik.errors[name];
+  return (
+    <Input
+      {...props}
+      value={formik.values[name]}
+      onChangeText={formik.handleChange(name)}
+      onBlur={formik.handleBlur(name)}
+      status={showError ? 'danger' : props.status}
+      caption={showError ? () => <Text category="c1" status="danger">{formik.errors[name]}</Text> : props.caption}
+      disabled={formik.isSubmitting || props.disabled}
+    />
+  );
+};
+
+export default FormInput;
