ffcc139dfab624fb9889529636810cae1d99bac7
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2022-03-02 18:56:39 +0200

    added to profile and image upload of profile

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const baseUrl = 'https://media.mw.metropolia.fi/wbma';
+export const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 export const media = 'Insert-name-here-media-Tag-dasdad';
 export const market = 'Insert-name-here-market-Tag-raijgaoi';
-export const avatar = 'Insert-name-here-avatar-Tag-nmagloikbng';
+export const avatarPic = 'Insert-name-here-avatar-Tag-';
