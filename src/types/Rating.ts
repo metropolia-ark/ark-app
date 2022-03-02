e0b847be1c5bfa0311a926db981f20819918d4e4
András Ádám <adm.andras@gmail.com>
2022-03-02 12:28:06 +0200

    Updated types and constants

---
+++ src/types/Rating.ts
@@ -0,0 +1,6 @@
+export interface Rating {
+  rating_id: number;
+  file_id: number;
+  user_id: number;
+  rating: 1;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,5 @@
+export * from './Comment';
+export * from './Media';
 export * from './navigation';
+export * from './Rating';
 export * from './User';
