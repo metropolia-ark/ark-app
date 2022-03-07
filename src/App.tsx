6e6a0a8817bf59152f1ad7d2bc1ef1ff0d1ff2bd
András Ádám <adm.andras@gmail.com>
2022-03-07 21:17:19 +0200

    Install toast message lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
+import Toast from 'react-native-toast-message';
 import { ApplicationProvider } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 import './utils/i18n';
@@ -12,6 +13,7 @@ const App = () => (
     <ApplicationProvider {...eva} theme={eva.light}>
       <Navigator />
       <StatusBar style="auto" />
+      <Toast />
     </ApplicationProvider>
   </AuthProvider>
 );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './constants';
 export * from './filter';
 export * from './i18n';
 export * from './request';
+export * from './toast';
