5142a4965e43dcc96a33a1ded6a5d79d6521cf9c
András Ádám <adm.andras@gmail.com>
2022-03-08 21:19:49 +0200

    Update tags

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ export const baseUrl = 'https://media.mw.metropolia.fi/wbma';
 export const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 
 // Media tags
-export const postTag = 'Insert-name-here-media-Tag-dasdad';
-export const petTag = 'Insert-name-here-market-Tag-raijgaoi';
-export const avatarTag = 'Insert-name-here-avatar-Tag-';
+export const postTag = 'ark-post-tag';
+export const petTag = 'ark-pet-tag';
+export const avatarTag = 'ark-avatar-tag-';
