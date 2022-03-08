034b321f6cae546c9ef7213593601e81a7862e32
András Ádám <adm.andras@gmail.com>
2022-03-08 22:42:57 +0200

    Update Spinner.tsx

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyleSheet, View } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 const Spinner = () => {
+  const animation = useRef<LottieView | null>();
+
+  // Handle playing and hiding animation
+  useEffect(() => {
+    animation.current?.play();
+    return () => animation.current?.reset();
+  }, []);
+
   return (
     <View style={styles.spinnerContainer}>
       <LottieView
+        ref={reference => animation.current = reference}
         style={styles.lottieSpinner}
         source={require('../../assets/animation/animal-care-loading.json')}
-        autoPlay={true}
-        loop={true}
+        loop
         speed={6}
       />
     </View>
